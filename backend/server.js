9e672a46972ebaed29b55f0cdeeb2372d764ae58
Atta ur Rehman <attaurr786@outlook.com>
2023-10-30 23:17:04 +0500

    nodemon installed

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,22 @@
 import  express  from "express";
+import products from "./data/products.js";
 const port = 5000;
 
 const app = express();
 
 app.get("/", (req,res) => {
-res.send("aPI is runnning")
+res.send("aPI is runnning");
+});
+app.get('/api/products' ,  (req,res) =>
+{
+    res.json(products)
+} );
 
+
+app.get('/api/products/:id' ,  (req,res) =>
+{
+    const pr = products.find((p) => p._id === req.params.id);
+    res.json(pr)
 });
 
 app.listen(port, () => console.log(`server runini gon port no ${port}`))
