6203d97e05521a73cd98729d0ca1ee99a931df3e
Atta ur Rehman <attaurr786@outlook.com>
2023-10-30 11:57:28 +0500

    Home Components Completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer';
+import HomeScree from './Screen/HomeScree';
 function App() {
 
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header/>
       <main className='py-3'>
 <Container>
+  <HomeScree></HomeScree>
       <h1>Wlcome to E-Shop</h1>
       </Container>
       </main>
